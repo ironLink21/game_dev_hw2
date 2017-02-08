dbf79838104494a73283fb8d9f826b468a40728e
Seth Bertlshofer <celestials2013@gmail.com>
2017-02-07 23:11:13 -0700

    creating mazes correctly

--- js/mazeGame.js
+++ js/mazeGame.js
@@ -10,9 +10,30 @@ var MazeGame = (()=>{
     var grid;
     var currCell;
     var context;
+    var maze = [];
     var frontiers = [];
 
-    function createGrid(size) {
+    function init(size = 5) {
+        let canvas = document.getElementById('canvas');
+        context = canvas.getContext('2d');
+
+        CanvasRenderingContext2D.prototype.clear = function() {
+            this.save();
+            this.setTransform(1, 0, 0, 1, 0, 0);
+            this.clearRect(0, 0, canvas.width, canvas.height);
+            this.restore();
+        };
+
+        initGrid(size);
+
+        let x = Math.floor((Math.random() * grid.length-1) + 1);
+        let y = Math.floor((Math.random() * grid.length-1) + 1);
+
+        currCell = createCell({location:{x, y}});
+        mark(currCell);
+    }
+
+    function initGrid(size) {
         grid = [];
         for(let i = 0; i < size; i++) {
             let cols = [];
@@ -27,18 +48,22 @@ var MazeGame = (()=>{
         let that = {};
 
         that.location = (specs.location) ? specs.location : console.log("missing location");
-        that.neighbors = (specs.neighbors) ? specs.neighbors : {};
+        that.directions = {N, E, S, W};
+        that.isStart = false;
+        that.isFinish = false;
+        that.isVisited = false;
+        that.isShortestPath = false;
 
         return that;
     }
 
     function mark(cell) {
-        // mark cell as in maze
         let x = cell.location.x;
         let y = cell.location.y;
-        grid[x][y] = cell;
 
-        // add surrounding cells as frontiers
+        grid[x][y] = cell;
+        maze.push(cell);
+
         addFrontier(x-1, y);
         addFrontier(x+1, y);
         addFrontier(x, y-1);
@@ -46,49 +71,41 @@ var MazeGame = (()=>{
     }
 
     function addFrontier(x, y) {
-        if(x >= 0 && y >= 0 && y < grid.length && x < grid.length && grid[x][y] === 0) {
-            let index = _.findIndex(frontiers,{x, y});
-            if(index === -1) {
-                grid[x][y] = 'F';
-                frontiers.push({x, y});
-            }
+        let index = _.findIndex(frontiers, {x, y});
+        if(x >= 0 && y >= 0 && y < grid.length && x < grid.length && index === -1 && grid[x][y] === 0) {
+            frontiers.push({x, y});
         }
+    }
+
+    function randomProperty(obj) {
+        let keys = Object.keys(obj);
+        let dir = keys[ keys.length * Math.random() << 0];
+        let cell = obj[dir];
+        return {dir, cell};
     }
 
     function findNeighbors(cell) {
-        let x = cell.x;
-        let y = cell.y;
-        let neighbors = [];
+        let x = cell.location.x;
+        let y = cell.location.y;
+        let neighbors = {};
 
-        if(x > 0 && grid[x-1][y] !== 0 && grid[x-1][y] !== 'F') {
-            neighbors.push({x:x-1, y});
+        if(y > 0 && _.findIndex(maze, grid[x][y-1]) !== -1) {
+            neighbors.N = {x, y:y-1};
         }
 
-        if(x+1 < grid.length && grid[x+1][y] !== 0 && grid[x+1][y] !== 'F') {
-            neighbors.push({x:x+1, y});
+        if(x+1 < grid.length && _.findIndex(maze, grid[x+1][y]) !== -1) {
+            neighbors.E = {x:x+1, y};
         }
 
-        if(y > 0 && grid[x][y-1] !== 0 && grid[x][y-1] !== 'F') {
-            neighbors.push({x, y:y-1});
+        if(y+1 < grid.length && _.findIndex(maze, grid[x][y+1]) !== -1) {
+            neighbors.S = {x, y:y+1};
         }
 
-        if(y+1 < grid.length && grid[x][y+1] !== 0 && grid[x][y+1] !== 'F') {
-            neighbors.push({x, y:y+1});
+        if(x > 0 && _.findIndex(maze, grid[x-1][y]) !== -1) {
+            neighbors.W = {x:x-1, y};
         }
 
         return neighbors;
-    }
-
-    function direction(cell1, cell2) {
-        let x = cell1.x;
-        let y = cell1.y;
-        let nx = cell2.x;
-        let ny = cell2.y;
-
-        if(y < ny) return N;
-        if(y > ny) return S;
-        if(x > nx) return E;
-        if(x < nx) return W;
     }
 
     function renderCell(cell) {
@@ -99,111 +116,80 @@ var MazeGame = (()=>{
             let x = cell.location.x;
             let y = cell.location.y;
 
-            if(!cell.neighbors.N) {
-                context.beginPath();
-                context.moveTo(x*w, y*h);
-                context.lineTo((x+1)*w, y*h);
-                context.stroke();
-            }
-
-            if(!cell.neighbors.E) {
-                context.beginPath();
-                context.moveTo((x+1)*w, y*h);
-                context.lineTo((x+1)*w, (y+1)*h);
-                context.stroke();
-            }
-
-            if(!cell.neighbors.S) {
-                context.beginPath();
-                context.moveTo((x+1)*w, (y+1)*h);
-                context.lineTo(x*w, (y+1)*h);
-                context.stroke();
-            }
-
-            if(!cell.neighbors.W) {
-                context.beginPath();
-                context.moveTo(x*w, (y+1)*h);
-                context.lineTo(x*w, y*h);
-                context.stroke();
-            }
-
             context.fillStyle = '#FFF';
             context.fillRect(x*w, y*h, w, h);
-        } else if (cell.type == 'F') {
-            context.fillStyle = cell.fill;
-            context.fillRect(cell.x*w, cell.y*h, w, h);
-            context.strokeStyle = '#000';
-            context.strokeRect(cell.x*w, cell.y*h, w, h);
 
-        } else {
-            context.fillStyle = cell.fill;
-            context.fillRect(cell.x*w, cell.y*h, w, h);
-            context.strokeStyle = '#000';
-            context.strokeRect(cell.x*w, cell.y*h, w, h);
+            _.each(cell.directions, (dir, index)=>{
+                switch(index) {
+                    case 'N':
+                        context.beginPath();
+                        context.moveTo(x*w, y*h);
+                        context.lineTo((x+1)*w, y*h);
+                        context.strokeStyle = (dir != 'N') ? '#FFF' : '#000';
+                        context.stroke();
+                        break;
+                    case 'E':
+                        context.beginPath();
+                        context.moveTo((x+1)*w, y*h);
+                        context.lineTo((x+1)*w, (y+1)*h);
+                        context.strokeStyle = (dir != 'E') ? '#FFF' : '#000';
+                        context.stroke();
+                        break;
+                    case 'S':
+                        context.beginPath();
+                        context.moveTo((x+1)*w, (y+1)*h);
+                        context.lineTo(x*w, (y+1)*h);
+                        context.strokeStyle = (dir != 'S') ? '#FFF' : '#000';
+                        context.stroke();
+                        break;
+                    case 'W':
+                        context.beginPath();
+                        context.moveTo(x*w, (y+1)*h);
+                        context.lineTo(x*w, y*h);
+                        context.strokeStyle = (dir != 'W') ? '#FFF' : '#000';
+                        context.stroke();
+                        break;
+                    default:
+                }
+            });
         }
     }
 
-    function render() {
+    function draw() {
         context.save();
 
         for(let x = 0; x < grid.length; x++) {
             for(let y = 0; y < grid.length; y++) {
                 let cell = grid[x][y];
-
-                if(cell === 0 || cell === 'F') {
-                    cell = (cell === 0) ? {type:cell, x, y, fill: 'rgba(128, 128, 128, 0.5)'} :{type:cell, x, y, fill: 'rgba(255, 0, 0, 0.5)'};
-                    renderCell(cell);
-                } else {
-                    renderCell(cell);
-                }
+                renderCell(cell);
             }
         }
 
         context.restore();
     }
 
-    function createMaze(size = 5) {
-        createGrid(size);
-
-        let x = Math.floor((Math.random() * grid.length-1) + 1);
-        let y = Math.floor((Math.random() * grid.length-1) + 1);
-
-        currCell = createCell({location:{x, y}});
-        mark(currCell);
-
+    function createMaze() {
         while(frontiers.length > 0) {
-            let frontier = frontiers.splice(Math.floor(Math.random() * frontiers.length), 1);
-            frontier = frontier[0];
-            let n = findNeighbors(frontier);
-            let neighbor = n.splice(Math.floor(Math.random() * n.length), 1);
-            neighbor = neighbor[0];
+            let nextCell = frontiers.splice(Math.floor(Math.random() * frontiers.length), 1)[0]; // find next sell from frontier cells
+            nextCell = createCell({location:nextCell});
+            mark(nextCell); // add cell to maze, move to cell
 
-            let dir = direction(frontier, neighbor);
+            let neighbor = randomProperty(findNeighbors(nextCell));
+
+            let dir = neighbor.dir;
             let dirOpp = OPPOSITE[dir];
 
-            let nextCell = createCell({location:frontier, neighbors:{[dirOpp]:neighbor}});
+            nextCell.directions[dir] = neighbor.cell;
             grid[nextCell.location.x][nextCell.location.y] = nextCell;
-
-            grid[currCell.location.x][currCell.location.y].neighbors[dir] = nextCell.location;
+            grid[neighbor.cell.x][neighbor.cell.y].directions[dirOpp] = nextCell.location;
 
             currCell = nextCell;
-
-            mark(nextCell);
-            render();
         }
+        draw();
     }
 
     window.onload=()=>{
-        let canvas = document.getElementById('canvas');
-        context = canvas.getContext('2d');
-
-        CanvasRenderingContext2D.prototype.clear = function() {
-            this.save();
-            this.setTransform(1, 0, 0, 1, 0, 0);
-            this.clearRect(0, 0, canvas.width, canvas.height);
-            this.restore();
-        };
-
-        createMaze(5);
+        init(15);
+        createMaze();
     };
 })();
